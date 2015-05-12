bf5b95e8d024a95ffdbbdfcf175bdb73499229dc
Rami Khalil <thesavagekoder@gmail.com>
2015-05-12 12:33:58 +0200

    Sphere Demo

---
+++ Unity Projects/Roll a Ball/Assets/Scripts/OneAxisFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OneAxisFollow : MonoBehaviour {
+	public GameObject followee;
+	public string axis;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		Vector3 tmp = transform.position;
+		if (axis.ToLower().Equals ("x"))
+			tmp.x = followee.transform.position.x;
+		else if (axis.ToLower().Equals ("y"))
+			tmp.y = followee.transform.position.y;
+		else if (axis.ToLower().Equals ("z"))
+			tmp.z = followee.transform.position.z;
+		transform.position = tmp;
+	}
+}
